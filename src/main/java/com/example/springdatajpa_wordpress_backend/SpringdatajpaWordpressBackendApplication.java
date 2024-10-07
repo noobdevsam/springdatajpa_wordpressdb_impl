de60864b822910620aea3489fcbf0bcf9be705e1
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-07 20:33:23 +0600

    first commit

---
+++ src/main/java/com/example/springdatajpa_wordpress_backend/SpringdatajpaWordpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_wordpress_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaWordpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaWordpressBackendApplication.class, args);
+	}
+
+}
